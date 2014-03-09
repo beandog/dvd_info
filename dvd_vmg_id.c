5876faf23ab98f16841098e16e6882481994cfa9
Steve Dibb <steve.dibb@gmail.com>
2014-03-09 12:33:53 -0600

    Remove copyright header from files during development, will add later in separate file

--- dvd_vmg_id.c
+++ dvd_vmg_id.c
@@ -8,26 +8,6 @@
 #include <dvdread/ifo_read.h>
 
 /**
- * Copyright (C) 2014 Steve Dibb <steve.dibb@gmail.com>
- *
- * See http://dvds.beandog.org
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
-/**
  * dvd_vmg_id.c
  * A simple little program to get the DVD VMG id.  It's entirely possible, and
  * common, that the string is blank.  If it's not blank, it is probably
