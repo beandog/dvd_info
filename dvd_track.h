cf3f0a031857619d9c40c44a34402893ebd88c65
Steve Dibb <steve.dibb@gmail.com>
2014-04-12 13:07:35 -0600

    Get MPEG video codec version

--- dvd_track.h
+++ dvd_track.h
@@ -1,3 +1,24 @@
 /**
  * Functions used to get information about a DVD track
  */
+
+/**
+ * Get the MPEG video codec version
+ *
+ * @param dvdread track IFO handler
+ * @retval 0 unknown
+ * @retval 1 MPEG-1
+ * @retval 2 MPEG-2
+ */
+int dvd_track_mpeg_version(ifo_handle_t *track_ifo) {
+
+	int version = track_ifo->vtsi_mat->vts_video_attr.mpeg_version;
+
+	if(version == 1)
+		return 1;
+	else if(version == 2)
+		return 2;
+	else
+		return 0;
+
+}
