33cb5bd25f2459f69f5b1c90f123dea6e534b7ee
Steve Dibb <steve.dibb@gmail.com>
2014-04-12 13:12:26 -0600

    Add helper functions to check for MPEG1, MPEG2 video codec

--- dvd_track.h
+++ dvd_track.h
@@ -22,3 +22,37 @@ int dvd_track_mpeg_version(ifo_handle_t *track_ifo) {
 		return 0;
 
 }
+
+/**
+ * Helper function to check if video codec is MPEG-1
+ *
+ * @param dvdread track IFO handler
+ * @return boolean
+ */
+bool dvd_track_mpeg1(ifo_handle_t *track_ifo) {
+
+	int version = track_ifo->vtsi_mat->vts_video_attr.mpeg_version;
+
+	if(version == 1)
+		return true;
+	else
+		return false;
+
+}
+
+/**
+ * Helper function to check if video codec is MPEG-1
+ *
+ * @param dvdread track IFO handler
+ * @return boolean
+ */
+bool dvd_track_mpeg2(ifo_handle_t *track_ifo) {
+
+	int version = track_ifo->vtsi_mat->vts_video_attr.mpeg_version;
+
+	if(version == 2)
+		return true;
+	else
+		return false;
+
+}
