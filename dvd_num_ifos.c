b0f2076b0634a47703c348081336d260cb8295cf
Steve Dibb <steve.dibb@gmail.com>
2014-04-18 16:30:03 -0600

    Simple script to check the number of IFOs on a DVD -- helpful for scripting

---
+++ dvd_num_ifos.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <dvdread/dvd_reader.h>
+#include <dvdread/ifo_read.h>
+
+void usage() {
+	fprintf(stderr, "Usage: dvd_num_ifos [dvd path]\n");
+}
+
+int main(int argc, char **argv) {
+
+	char *dvd;
+	int num_title_sets;
+
+	dvd = argv[1];
+
+	if(argc == 1) {
+		dvd = "/dev/dvd";
+	} else if(argc == 2) {
+		dvd = argv[1];
+	} else {
+		usage();
+		return 1;
+	}
+
+	dvd_reader_t *dvdread_dvd;
+	dvdread_dvd = DVDOpen(dvd);
+
+	if(!dvdread_dvd) {
+		printf("dvd_qa_ifo: DVDOpen(%s) died!\n", dvd);
+		return 1;
+	}
+
+	// Open IFO zero
+	ifo_handle_t *ifo_zero;
+	ifo_zero = ifoOpen(dvdread_dvd, 0);
+	if(!ifo_zero) {
+		DVDClose(dvdread_dvd);
+		return 1;
+	}
+
+	num_title_sets = ifo_zero->vmgi_mat->vmg_nr_of_title_sets;
+	ifoClose(ifo_zero);
+
+	printf("%d\n", num_title_sets);
+
+	DVDClose(dvdread_dvd);
+
+	return 0;
+
+}
