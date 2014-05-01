54bd7b39d4771e7dc05457db4e47e8e9c81ea04f
Steve Dibb <steve.dibb@gmail.com>
2014-05-01 11:24:01 -0600

    Add function documentation

--- dvd_track_audio.h
+++ dvd_track_audio.h
@@ -2,4 +2,15 @@
 #include <string.h>
 #include <dvdread/ifo_types.h>
 
+/**
+ * Get the audio language code for a track.  A two-character string that is a
+ * short name for a language.
+ *
+ * Examples: en: English, fr: French, es: Spanish
+ *
+ * @param track_ifo dvdread track IFO handler
+ * @param audio_track audio track number
+ * @param p string pointer
+ * @return success
+ */
 int dvd_track_audio_lang_code(const ifo_handle_t *track_ifo, int audio_track, char *p);
