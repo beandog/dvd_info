d496036b4af51110ddb5c7975f6ce92d2d8a1a18
Steve Dibb <steve.dibb@gmail.com>
2014-07-24 12:33:16 -0600

    Remove setting variables from functions that are short

--- dvd_device.c
+++ dvd_device.c
@@ -10,11 +10,7 @@
 
 bool dvd_device_access(const char *device_filename) {
 
-	int device_access;
-
-	device_access = access(device_filename, F_OK);
-
-	if(device_access == 0)
+	if(access(device_filename, F_OK) == 0)
 		return true;
 	else
 		return false;
@@ -24,44 +20,28 @@ bool dvd_device_access(const char *device_filename) {
 // Returns DVD device file descriptor
 int dvd_device_open(const char *device_filename) {
 
-	int dvd_fd;
-
-	dvd_fd = open(device_filename, O_RDONLY | O_NONBLOCK);
-
-	return dvd_fd;
+	return open(device_filename, O_RDONLY | O_NONBLOCK);
 
 }
 
 int dvd_device_close(const int dvd_fd) {
 
-	int dvd_close;
-
-	dvd_close = close(dvd_fd);
-
-	return dvd_close;
+	return close(dvd_fd);
 
 }
 
 bool dvd_device_is_hardware(const char *device_filename) {
 
-	bool is_hardware;
-	is_hardware = false;
-
-	if(strncmp(device_filename, "/dev/", 5) == 0) {
-		is_hardware = true;
-	}
-
-	return is_hardware;
+	if(strncmp(device_filename, "/dev/", 5) == 0)
+		return true;
+	else
+		return false;
 
 }
 
 bool dvd_device_is_image(const char *device_filename) {
 
-	bool is_hardware;
-
-	is_hardware = dvd_device_is_hardware(device_filename);
-
-	if(is_hardware)
+	if(dvd_device_is_hardware(device_filename))
 		return false;
 	else
 		return true;
