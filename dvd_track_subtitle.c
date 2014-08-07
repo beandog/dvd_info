df1db9984429d515923f5ff5b0907641c6bdf2c2
Steve Dibb <steve.dibb@gmail.com>
2014-08-07 17:42:40 -0600

    Add FIXME notes

--- dvd_track_subtitle.c
+++ dvd_track_subtitle.c
@@ -2,6 +2,11 @@
 
 // TODO this function could stand a lot of strict error checking to see what the subtitle status is, and offer different return codes
 // Have dvd_debug check for issues here.
+// FIXME use isalpha()
+// FIXME I want to have some kind of distinguishment in here, and for audio tracks
+// if it's an invalid language.  If it's missing one, set it to unknown (for example)
+// but if it's invalid, maybe guess that it's in English, or something?  Dunno.
+// Having a best-guess approach might not be bad, maybe even look at region codes
 int dvd_track_subtitle_lang_code(const ifo_handle_t *vts_ifo, const int subtitle_track, char *p) {
 
 	char lang_code[3] = {'\0'};
