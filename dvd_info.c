9cb0c93980848b31fa7eefe04e04bc716b73bcb4
Steve Dibb <steve.dibb@gmail.com>
2014-09-23 11:43:27 -0600

    Move declarations into header file

--- dvd_info.c
+++ dvd_info.c
@@ -19,8 +19,6 @@
 #include "dvd_vmg_ifo.h"
 #include "dvd_track.h"
 
-void print_usage(char *binary);
-
 void print_usage(char *binary) {
 
 	printf("Usage: %s [options] [-t track number] [dvd path]\n", binary);
@@ -43,76 +41,6 @@ void print_usage(char *binary) {
 	printf("See 'man dvd_info' for more details, or http://dvds.beandog.org/\n");
 
 }
-
-struct dvd_info {
-	uint16_t video_title_sets;
-	uint8_t side;
-	char title[DVD_TITLE + 1];
-	char provider_id[DVD_PROVIDER_ID + 1];
-	char vmg_id[DVD_VMG_ID + 1];
-	uint16_t tracks;
-	uint16_t longest_track;
-};
-
-struct dvd_video {
-	char codec[DVD_VIDEO_CODEC + 1];
-	char format[DVD_VIDEO_FORMAT + 1];
-	char aspect_ratio[DVD_VIDEO_ASPECT_RATIO + 1];
-	uint16_t width;
-	uint16_t height;
-	bool letterbox;
-	bool pan_and_scan;
-	uint8_t df;
-	char fps[DVD_VIDEO_FPS + 1];
-	uint8_t angles;
-};
-
-struct dvd_audio {
-	uint8_t track;
-	uint8_t active;
-	char stream_id[DVD_AUDIO_STREAM_ID + 1];
-	char lang_code[DVD_AUDIO_LANG_CODE + 1];
-	char codec[DVD_AUDIO_CODEC + 1];
-	uint8_t channels;
-};
-
-struct dvd_subtitle {
-	uint8_t track;
-	uint8_t active;
-	char stream_id[DVD_SUBTITLE_STREAM_ID + 1];
-	char lang_code[DVD_SUBTITLE_LANG_CODE + 1];
-};
-
-struct dvd_chapter {
-	uint8_t chapter;
-	char length[DVD_CHAPTER_LENGTH + 1];
-	uint32_t msecs;
-	uint8_t startcell;
-};
-
-struct dvd_cell {
-	uint8_t cell;
-	char length[DVD_CELL_LENGTH + 1];
-	uint32_t msecs;
-};
-
-struct dvd_track {
-	uint16_t track;
-	uint16_t vts;
-	uint8_t ttn;
-	char vts_id[DVD_TRACK_VTS_ID + 1];
-	char length[DVD_TRACK_LENGTH + 1];
-	uint32_t msecs;
-	uint8_t chapters;
-	uint8_t audio_tracks;
-	uint8_t subtitles;
-	uint8_t cells;
-	struct dvd_video dvd_video;
-	struct dvd_audio *dvd_audio_tracks;
-	struct dvd_subtitle *dvd_subtitles;
-	struct dvd_chapter *dvd_chapters;
-	struct dvd_cell *dvd_cells;
-};
 
 int main(int argc, char **argv) {
 
