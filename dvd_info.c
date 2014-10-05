9ea573bb6ffa4eb9cc6e0c062d8a23b1a942557a
Steve Dibb <steve.dibb@gmail.com>
2014-10-05 14:05:35 -0600

    Also check if IFO is a VMG

--- dvd_info.c
+++ dvd_info.c
@@ -328,7 +328,7 @@ int main(int argc, char **argv) {
 
 	// Open VMG IFO -- where all the cool stuff is
 	vmg_ifo = ifoOpen(dvdread_dvd, 0);
-	if(!vmg_ifo) {
+	if(!vmg_ifo || !ifo_is_vmg(vmg_ifo)) {
 		fprintf(stderr, "dvd_info: Opening VMG IFO failed\n");
 		DVDClose(dvdread_dvd);
 		return 1;
