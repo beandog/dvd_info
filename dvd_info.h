f0571a4d95cb08fd5fd68a47d4cf478f935c420d
Steve Dibb <steve.dibb@gmail.com>
2014-10-24 22:19:03 -0600

    Set default devices for *BSD

--- dvd_info.h
+++ dvd_info.h
@@ -1,4 +1,17 @@
+// Default DVD device
+#ifdef __linux__
+#define DEFAULT_DVD_DEVICE "/dev/sr0"
+#elif __FreeBSD__ || __DragonFly__
+#define DEFAULT_DVD_DEVICE "/dev/acd0"
+#elif __NetBSD__
+#define DEFAULT_DVD_DEVICE "/dev/cd0d"
+#elif __OpenBSD__
+#define DEFAULT_DVD_DEVICE "/dev/cd0c"
+#elif __APPLE__ && __MACH__
+#define DEFAULT_DVD_DEVICE "/dev/disk1"
+#else
 #define DEFAULT_DVD_DEVICE "/dev/dvd"
+#endif
 
 // String lengths for DVD metadata
 #define DVD_TITLE 32
