55961beda7c7fa94d69f093dbe19fda55609f0cf
Steve Dibb <steve.dibb@gmail.com>
2014-11-28 16:38:37 -0700

    Use VMGI MAT

--- dvd_vmg_ifo.c
+++ dvd_vmg_ifo.c
@@ -121,7 +121,7 @@ const char *dvd_specification_version(const ifo_handle_t *vmg_ifo) {
 
 	if(ifo_is_vmg(vmg_ifo)) {
 		char str[DVD_SPECIFICATION_VERSION + 1] = {'\0'};
-		snprintf(str, DVD_SPECIFICATION_VERSION + 1, "%01x.%01x", vmg_ifo->vtsi_mat->specification_version >> 4, vmg_ifo->vtsi_mat->specification_version & 0xf);
+		snprintf(str, DVD_SPECIFICATION_VERSION + 1, "%01x.%01x", vmg_ifo->vmgi_mat->specification_version >> 4, vmg_ifo->vmgi_mat->specification_version & 0xf);
 		return strndup(str, DVD_SPECIFICATION_VERSION + 1);
 	} else
 		return "";
