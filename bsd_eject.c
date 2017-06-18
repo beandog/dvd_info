67adb7c265cc7a2b24157a185240a77cb15038d4
Steve Dibb <steve.dibb@gmail.com>
2017-06-18 17:49:19 -0600

    Cleanup code

--- bsd_eject.c
+++ bsd_eject.c
@@ -61,25 +61,20 @@ int main(int argc, char **argv) {
 		return 1;
 	}
 
-	int retval = 0;
         if(eject_device) {
-#if defined (__FreeBSD__) || defined (__OpenBSD__)
+
+		int retval = 0;
 		retval = ioctl(cdrom, CDIOCEJECT);
-#elif defined (__NetBSD__)
-		int arg = 0;
-		retval = ioctl(cdrom, DIOCEJECT, &arg);
-#else
-		retval = ioctl(cdrom, CDIOCEJECT);
-#endif
-	} else {
-		retval = ioctl(cdrom, CDIOCCLOSE);
+
+		if(retval < 0) {
+			printf("retval: %i\n", retval);
+			printf("errno: %i\n", errno);
+			close(cdrom);
+			return 1;
+		}
+
 	}
 	
-	if(retval < 0) {
-		printf("retval: %i\n", retval);
-		printf("errno: %i\n", errno);
-		return 1;
-	}
 
 	close(cdrom);
 
@@ -93,6 +88,6 @@ void print_usage(void) {
 	printf("	-h	this help output\n");
 	printf("	-t	close tray instead of opening\n");
 	printf("\n");
-	printf("If no device is given, default is %s\n", DEFAULT_DVD_DEVICE);
+	printf("Default DVD device is %s\n", DEFAULT_DVD_DEVICE);
 
 }
