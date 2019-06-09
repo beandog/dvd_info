669d2b3172c1c302b57b2a478a3c80068ad27540
Steve Dibb <steve.dibb@gmail.com>
2019-06-08 21:41:15 -0600

    Add structs for dvd_player

--- dvd_mpv.h
+++ dvd_mpv.h
@@ -1,6 +1,25 @@
 #ifndef DVD_INFO_MPV_H
 #define DVD_INFO_MPV_H
 
+struct dvd_player {
+	char config_dir[PATH_MAX];
+	char mpv_config_dir[PATH_MAX];
+};
+
+struct dvd_playback {
+	uint16_t track;
+	uint8_t first_chapter;
+	uint8_t last_chapter;
+	bool fullscreen;
+	bool deinterlace;
+	char audio_lang[3];
+	char audio_stream_id[4];
+	bool subtitles;
+	char subtitles_lang[3];
+	char subtitles_stream_id[4];
+	char mpv_chapters_range[32];
+};
+
 struct dvd_trip {
 	uint16_t track;
 	uint8_t first_chapter;
