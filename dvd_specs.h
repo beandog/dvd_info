a3b07a1c9fec5693f1c6e3b5b130dcdc364ab997
Steve Dibb <steve.dibb@gmail.com>
2019-06-16 22:01:34 -0600

    Add notes about max sizes

--- dvd_specs.h
+++ dvd_specs.h
@@ -1,6 +1,16 @@
 #ifndef DVD_INFO_SPECS_H
 #define DVD_INFO_SPECS_H
 
+// The max size of a dual-layer disc is 8.5 GB
+// or, 1024 * 1024 * 1024 * 8.5
+// The value falls into an int64_t range
+#define DVD_MAX_BYTES 9126805504
+
+// DVD storage is in blocks, with one being 2048 bytes
+// 9126805504 / 2048
+// The value falls into an int32_t range
+#define DVD_MAX_BLOCKS 4456448
+
 #define DVD_TITLE 32
 #define DVD_PROVIDER_ID 32
 #define DVD_VMG_ID 12
