135b51138235f845c702fc17ade500c990363b3c
Steve Dibb <steve.dibb@gmail.com>
2019-06-17 22:21:56 -0600

    Add a check on DVDFileStat()

--- dvd_vts.c
+++ dvd_vts.c
@@ -28,9 +28,9 @@ uint64_t dvd_vts_blocks(dvd_reader_t *dvdread_dvd, const uint16_t vts_number) {
 uint64_t dvd_vts_filesize(dvd_reader_t *dvdread_dvd, const uint16_t vts_number) {
 
 	uint64_t vts_blocks = 0;
-	uint64_t vts_filesize = 0;
-
 	vts_blocks = dvd_vts_blocks(dvdread_dvd, vts_number);
+
+	uint64_t vts_filesize = 0;
 	vts_filesize = vts_blocks * DVD_VIDEO_LB_LEN;
 
 	return vts_filesize;
@@ -40,10 +40,13 @@ uint64_t dvd_vts_filesize(dvd_reader_t *dvdread_dvd, const uint16_t vts_number) 
 int dvd_vts_vobs(dvd_reader_t *dvdread_dvd, const uint16_t vts_number) {
 
 	dvd_stat_t dvdread_stat;
+
+	int retval = 0;
+	retval = DVDFileStat(dvdread_dvd, vts_number, DVD_READ_TITLE_VOBS, &dvdread_stat);
+	if(retval < 0)
+		return 0;
+
 	int vts_vobs = 0;
-
-	DVDFileStat(dvdread_dvd, vts_number, DVD_READ_TITLE_VOBS, &dvdread_stat);
-
 	vts_vobs = dvdread_stat.nr_parts;
 
 	return vts_vobs;
