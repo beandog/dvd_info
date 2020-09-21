23dbca584188a0c9047a9de0c47b4ea59f30b7a3
Steve Dibb <steve.dibb@gmail.com>
2020-09-21 02:13:19 -0600

    Add missing prototype

--- dvd_open.h
+++ dvd_open.h
@@ -12,6 +12,8 @@
 #include "dvd_vmg_ifo.h"
 #include "dvd_info.h"
 
+int device_open(const char *device_filename);
+
 struct dvd_info dvd_info_open(dvd_reader_t *dvdread_dvd, const char *device_filename);
 
 #endif
