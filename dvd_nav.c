90a17240b0807a881db0b4bc9cb3f36e8020ad51
Steve Dibb <steve.dibb@gmail.com>
2026-02-20 08:37:25 -0700

    Add libdvdnav header, code

---
+++ dvd_nav.c
@@ -0,0 +1,23 @@
+#include "dvd_nav.h"
+
+/**
+ * Use libdvnav to get the DVD region.
+ *
+ * Possible regions are 0 (region-free) or 1 through 8
+ * See https://en.wikipedia.org/wiki/DVD_region_code for region codes
+ *
+ * If the value is out-of that range, return 0.
+ */
+int32_t dvd_info_region(dvdnav_t *dvdnav_dvd) {
+
+	int32_t dvdnav_region;
+	int32_t dvdnav_region_mask;
+
+	dvdnav_region = dvdnav_get_region_mask(dvdnav_dvd, &dvdnav_region_mask);
+
+	if(dvdnav_region < 0 || dvdnav_region > 8)
+		return 0;
+
+	return dvdnav_region;
+
+}
---
+++ dvd_nav.h
@@ -0,0 +1,8 @@
+#ifndef DVD_INFO_DVD_NAV_H
+#define DVD_INFO_DVD_NAV_H
+
+#include <dvdnav/dvdnav.h>
+
+int32_t dvd_info_region(dvdnav_t *dvdnav_dvd);
+
+#endif
